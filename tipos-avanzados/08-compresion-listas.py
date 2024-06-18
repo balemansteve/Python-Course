ea471e93955c30440fae52f4125331aeb0b8e829
balemansteve <balemansteve@gmail.com>
2024-06-17 22:46:10 -0300

    Filtrar y transformar con lambda

--- tipos-avanzados/08-compresion-listas.py
+++ tipos-avanzados/08-compresion-listas.py
@@ -10,14 +10,21 @@ usuarios = [
 #     nombres.append(usuario[0])
 # print(nombres)
 
-# Transformacion
 # nombres = [expresion for i in items]
+
+# Transformacion - map
 # nombres = [usuario[1] for usuario in usuarios]
 
-# Filtramos
+# Filtramos - filter
 # nombres = [usuario for usuario in usuarios if usuario[1] > 2]
 
 # Filtrar y transformar
-nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
-nombres.sort(reverse=True)
-print(nombres)
+# nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
+
+# Filtrar y transformar - con lambda usando la funcion map y filter
+# nombres = list(map(lambda usuario: usuario[0], usuarios))
+
+menosUsuario = list(filter(lambda usuario: usuario[1] > 2, usuarios))
+print(menosUsuario)
+
+# print(nombres)
