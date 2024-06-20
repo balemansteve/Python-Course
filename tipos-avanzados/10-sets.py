c0d5f815f9f8ae4e985050b4afdb4dd5c9fb9783
balemansteve <balemansteve@gmail.com>
2024-06-20 01:47:45 -0300

    sets

---
+++ tipos-avanzados/10-sets.py
@@ -0,0 +1,26 @@
+# set significa grupo o conjunto
+# solo hay una instancia de cada elemento
+primer = {1, 1, 2, 3, 4}
+# primer.add(5)
+# # primer.remove(1)
+print(primer)
+
+# Crea un set a partir de una lista
+segundo = [3, 4, 5]
+segundo = set(segundo)
+print(segundo)
+
+# imprime la union de los dos sets (operador union '|')
+print(primer | segundo)
+
+# imprime la interseccion de los dos sets (operador interseccion '&')
+# son los elementos que estan en ambos sets
+print(primer & segundo)
+
+# imprime la diferencia de los dos sets (operador diferencia '-')
+# son los elementos que estan en el primer set pero no en el segundo, es decir que quita del primer set los elementos que esten en el segundo
+print(primer - segundo)
+
+# imprime la diferencia simetrica de los dos sets (operador diferencia simetrica '^')
+# son los elementos que no se repiten en ambos sets
+print (primer ^ segundo)
