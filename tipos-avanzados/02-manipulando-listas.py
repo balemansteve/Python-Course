f309207a5a3e95fbdfd537ec52782af686c99968
balemansteve <balemansteve@gmail.com>
2024-06-26 00:57:15 -0300

    Manipulando listas

--- tipos-avanzados/02-manipulando-listas.py
+++ tipos-avanzados/02-manipulando-listas.py
@@ -1,9 +1,20 @@
+# Declara una lista con 4 elementos
 mascotas = ['perro', 'gato', 'pez', 'tortuga']
-print(mascotas[0])
-mascotas[0] = "Bicho"
-# print(mascotas)
-# print(mascotas[::2])
-# print(mascotas[-1])
 
+# Imprime el primer elemento de la lista
+print(mascotas[0])
+
+# Cambia el primer elemento de la lista
+mascotas[0] = "Bicho"
+print(mascotas)
+
+# Imprime los ultimos dos elementos de la lista
+print(mascotas[::2])
+
+# Imprime el ultimo elemento de la lista
+print(mascotas[-1])
+
+# Declara una lista con los numeros del 0 al 20
 numeros = list(range(21))
+# Imprime los numeros pares de la lista
 print(numeros[2::2])
