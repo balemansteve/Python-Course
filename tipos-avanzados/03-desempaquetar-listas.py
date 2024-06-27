717cd4f2896392208434baacd74bb1b3445da22d
balemansteve <balemansteve@gmail.com>
2024-06-27 00:20:38 -0300

    Desempaquetar listas

--- tipos-avanzados/03-desempaquetar-listas.py
+++ tipos-avanzados/03-desempaquetar-listas.py
@@ -1,13 +1,17 @@
+# Declaramos una lista con 5 números
 numeros = [1, 2, 3, 4, 5]
 
-# feo!
+# Asi no, aqui lo que ocurre es que se crea una lista para cada variable.
 # primero = [0]
 # segundo = [1]
 # tercero = [2]
 
+# Desempaquetamos la lista en tres variables pero da error si no hay la misma cantidad de variables que de elementos en la lista
 # primero, segundo, tercero = numeros
 # print(primero, segundo, tercero)
 
-primero, *otros, penu, ultimo = numeros
-print(primero, penu, ultimo)
+# Desempaquetamos la lista en tres variables y el resto de los elementos en una lista llamada otros
+primero, *otros, penultimo, ultimo = numeros
+# Improprime los valores de las variables
+print(primero, penultimo, ultimo)
 print(otros)
