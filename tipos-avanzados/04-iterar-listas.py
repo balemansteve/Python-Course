3c96c2c335eb13ef06964e21d0d2bd3e02c11c05
balemansteve <balemansteve@gmail.com>
2024-07-02 01:51:57 -0300

    iterar listas

--- tipos-avanzados/04-iterar-listas.py
+++ tipos-avanzados/04-iterar-listas.py
@@ -1,6 +1,8 @@
+# Declaramos una lista con 5 elementos
 mascotas = ['perro', 'gato', 'loro', 'pez', 'conejo']
 
-
+# Iteramos la lista de mascotas
+# Enumerate nos devuelve el indice y el valor de la lista
 for indice, mascota in enumerate(mascotas):
+    # Imprimimos el indice y el valor de la lista
     print(indice, mascota)
-    
