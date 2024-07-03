8dca12c3431f9a244d75f46399265c540fa59848
balemansteve <balemansteve@gmail.com>
2024-07-03 01:52:01 -0300

    buscar elemento

--- tipos-avanzados/05-buscar-elemento.py
+++ tipos-avanzados/05-buscar-elemento.py
@@ -1,5 +1,9 @@
+# Declaramos una lista con 6 elementos
 mascotas = ['perro', 'pez', 'gato', 'loro', 'pez', 'conejo']
 
+# count nos permite contar cuantas veces se repite un elemento en la lista
 print(mascotas.count('pez'))
+
+# index nos permite buscar un elemento en la lista pero solo nos devuelve el primer indice donde se encuentra
 if "pez" in mascotas:
     print(mascotas.index("pez"))
