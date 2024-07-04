c89af4573c89eaf7e6ee52827a4ccc95336b1d63
balemansteve <balemansteve@gmail.com>
2024-07-04 13:44:25 -0300

    agregando eliminando

--- tipos-avanzados/06-agregando-eliminando.py
+++ tipos-avanzados/06-agregando-eliminando.py
@@ -1,3 +1,4 @@
+# Declara una lista de 6 elementos
 mascotas = [
     'gatito',
     'perro',
@@ -6,11 +7,27 @@ mascotas = [
     'pez',
     'conejo'
 ]
-mascotas.insert(1, "Melvin")
-mascotas.append("conejita")
 
+# Agrega un elemento en el indice 1 de la lista
+mascotas.insert(1, "Melvin")
+print(mascotas)
+
+# Agrega un elemento al final de la lista
+mascotas.append("conejita")
+print(mascotas)
+
+# Elimina el primer elemento que coincida con el valor dado
 mascotas.remove("pez")
+print(mascotas)
+
+# Elimina el elemento en el indice dado
 mascotas.pop(1)
-del mascotas[0]
+print(mascotas)
+
+# Elimina el elemento en el indice dado
+del mascotas[1]
+print(mascotas)
+
+# Elimina todos los elementos de la lista
 mascotas.clear()
 print(mascotas)
