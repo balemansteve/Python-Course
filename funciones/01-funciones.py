05f822c22ac25f3faa791a51fbac946cf39faf80
balemansteve <balemansteve@gmail.com>
2024-07-29 22:50:15 -0300

    funciones

--- funciones/01-funciones.py
+++ funciones/01-funciones.py
@@ -1,17 +1,13 @@
+# Definir una función que reciba un nombre y un apellido, y que imprima un mensaje de bienvenida. El apellido debe tener un valor por defecto de "Feliz". Llamar a la función con un solo argumento y con dos argumentos.
 def hola(nombre, apellido="Feliz"):
     print("Hola, mundo!")
     print(f"Bienvenido {nombre} {apellido}")
 
-
+# Llamar a la función con dos argumentos, el segundo sobreescribe el valor por defecto
 hola("Bryan", "Aleman")
+
+# Llamar a la funcion con un solo argumento, el segundo toma el valor por defecto
 hola("Ombligo")
 
-
-
-
-
-
-
-
-
+# Llamar a la funcion con argumentos nombrados, aunque no se respete el orden, se asignan a los argumentos correspondientes
 hola(apellido="Aleman", nombre="Bryan")
