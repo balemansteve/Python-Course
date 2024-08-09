823afeeceebcaf420b7bbfcde2ef17d4ba8c70a7
balemansteve <balemansteve@gmail.com>
2024-08-08 23:59:21 -0300

    kwargs

--- funciones/03-kwargs.py
+++ funciones/03-kwargs.py
@@ -1,6 +1,7 @@
+# Definimos una función que recibe un número indeterminado de argumentos
 def get_product(**datos):
     print(datos["id"], datos["nombre"])
     
-    
+# Llamamos a la función con diferentes cantidades de argumentos
 get_product(id="23", nombre="iphone", precio="50")
     
