680e5c31c7d97d3f658384d3776044ab96081a47
balemansteve <balemansteve@gmail.com>
2024-08-13 23:57:13 -0300

    return

--- funciones/04-return.py
+++ funciones/04-return.py
@@ -1,8 +1,9 @@
+# Definimos una funcion que recibe dos argumentos y devuelve la suma de ambos
 def suma(a, b):
     resultado = a + b
     return resultado
 
-
+# Llamamos a la función que definimos anteriormente para sumar dos números y guardamos el resultado en una variable
 c = suma(1, 2)
 d = suma(c, 2)
 
