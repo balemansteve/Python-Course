2a0db0e11d789de921df18c88409978879a3ed53
balemansteve <balemansteve@gmail.com>
2024-08-14 23:26:38 -0300

    alcance

--- funciones/05-alcance.py
+++ funciones/05-alcance.py
@@ -1,11 +1,12 @@
+# Definimos una variable global
 saludo = "Hola Python"
 
-
+# Definimos una función que modifica la variable global
 def saludar():
     global saludo
     saludo = "Hola Mundo"
     
-    
+# Definimos una función que no modifica la variable global
 def saludaChanchito():
     saludo = "Hola Chanchito"
 
