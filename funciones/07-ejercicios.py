86b2045abfe2fb7ba086b172afa9e8efe92b3a8a
balemansteve <balemansteve@gmail.com>
2024-08-19 22:23:50 -0300

    ejercicio palindromo

--- funciones/07-ejercicios.py
+++ funciones/07-ejercicios.py
@@ -1,16 +1,17 @@
+# Definimos una funcion que nos dice si un texto es palindromo (que se lee igual al derecho que al reves)
 def es_palindromo(texto):
-    texto_sin_espacios = ""
+    texto_sin_espacios = "" # Variable que almacenara el texto sin espacios
     for i in texto:
         if i == " ": continue
         else:
-            texto_sin_espacios += i.lower()
-    texto_reversa = ""
+            texto_sin_espacios += i.lower() # Agregamos el caracter iterado, en minuscula con la funcion lower()
+    texto_reversa = "" # Variable que almacenara el texto_sin_espacios en reversa
     for i in texto_sin_espacios[::-1]:
         texto_reversa += i
-    return texto_sin_espacios == texto_reversa
+    return texto_sin_espacios == texto_reversa # Retorna un booleando comprobando si es True o False que el texto_sin_espacios sea igual a texto_reversa y sea palindromo
             
     
-    
+# Imprimimos varios textos para comprobar si son palindromos gracias a la funcion que definimos
 print("Abba", es_palindromo("Abba"))
 print("Reconocer", es_palindromo("Reconocer"))
 print("Amo la paloma", es_palindromo("Amo la paloma"))
