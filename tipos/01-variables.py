326668cd29e1daccca1e01729f010974b8148948
balemansteve <balemansteve@gmail.com>
2024-08-20 23:44:08 -0300

    variables

--- tipos/01-variables.py
+++ tipos/01-variables.py
@@ -1,8 +1,15 @@
+# Definimos variables con distintas semánticas en sus nombres, mostrando que Python es case-sensitive (distingue minusculas de mayusculas)
 nombre_curso = "Ultimate Python"
 nombre1 = "Ultimate Python"
 noMbre_curso = "Mundo"
+
+# Imprimimos dos variables para mostrar el case-sentitive
 print(nombre_curso, noMbre_curso)
+
+# Definimos variables con distintos tipos de datos, int, float y bool
 alumnos = 5000
 puntaje = 9.9
 publicado = True
-  
+
+# Imprimimos las variables y sus tipos de datos usando la funcion inegrada type()
+print(type(alumnos), type(puntaje), type(publicado))
