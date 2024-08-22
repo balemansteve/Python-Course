cf67c115d5dcaa2d175416c4dd54b1ca0a4c8d10
balemansteve <balemansteve@gmail.com>
2024-08-21 23:59:12 -0300

    strings

--- tipos/02-strings.py
+++ tipos/02-strings.py
@@ -1,11 +1,17 @@
+# Definimos una variable tipo string
 nombre_curso = "Ultimate Python"
-descripcion_curso = """
-Ultimate Python, este curso contempla todo lo que necesitas
-saber para trabajar como programador
-"""
+
+# Definimos una variable tipo string con saltos de linea (agregando """ al inicio y al final)
+descripcion_curso = """Ultimate Python, este curso contempla todo lo que necesitas 
+saber para trabajar como programador"""
+
+# Realizamos varios tipos de impresiones de la variable nombre_curso
 print(len(nombre_curso))
 print(nombre_curso[0])
 print(nombre_curso[0:8])
 print(nombre_curso[9:])
 print(nombre_curso[:8])
 print(nombre_curso[:])
+
+# Imprimimos la variable descripcion_curso y se pueden ver los saltos de linea
+print(descripcion_curso)
