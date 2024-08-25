5f77e30a700772abf87e10bb6b1061f21edd22f6
balemansteve <balemansteve@gmail.com>
2024-08-25 00:09:03 -0300

    metodos-strings

--- tipos/04-metodos-strings.py
+++ tipos/04-metodos-strings.py
@@ -2,14 +2,14 @@
 animal = "  chanCHito feliz  "
 
 # Imprimimos varios metodos que trabajan con strings (objeto.metodo())
-print(animal.upper())
-print(animal.lower())
-print(animal.strip().capitalize())
-print(animal.title())
-print(animal.strip())
-print(animal.lstrip())
-print(animal.rstrip())
-print(animal.find("cH"))
-print(animal.replace("nCH", "j"))
-print("nCH" in animal)
-print("nCH" not in animal)
+print(animal.upper()) # Convierte todos los caracteres del string a mayúsculas
+print(animal.lower()) # Convierte todos los caracteres del string a minúsculas
+print(animal.strip().capitalize()) # strip() Elimina los espacios en blanco al inicio y al final del string y capitalize() convierte la primera letra a mayúscula
+print(animal.title()) # Convierte la primera letra de cada palabra a mayúscula
+print(animal.strip()) # Elimina los espacios en blanco al inicio y al final del string
+print(animal.lstrip()) # Elimina los espacios en blanco al inicio del string
+print(animal.rstrip()) # Elimina los espacios en blanco al final del string
+print(animal.find("cH")) # Encuentra la posición donde aparece "cH", o -1 si no lo encuentra
+print(animal.replace("nCH", "j")) # Reemplaza "nCH" por "j" en el string
+print("nCH" in animal) # Verifica si "nCH" está en el string y retorna True o False
+print("nCH" not in animal) # Verifica si "nCH" no está en el string y retorna True o False
