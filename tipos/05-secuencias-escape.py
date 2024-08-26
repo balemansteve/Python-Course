3681519bd81fa950ebd4f759e32a8837afe5020c
balemansteve <balemansteve@gmail.com>
2024-08-26 00:00:08 -0300

    secuencias-escape

--- tipos/05-secuencias-escape.py
+++ tipos/05-secuencias-escape.py
@@ -1,8 +1,9 @@
-# Secuencias de escape
-# \"
-# \'
-# \\
-# \n
+# Secuencias de escape: Son combinaciones de caracteres que representan algo especial en un string, como saltos de línea o comillas.
+# \" inserta una comilla doble dentro de un string
+# \' inserta una comilla simple dentro de un string
+# \\ inserta una barra invertida dentro de un string
+# \n representa un salto de línea
 
-curso = "Ultimate \nPython\""
+# Definimos una variable que almacena un string con comillas dobles al principio y al final, y un salto de línea en el medio
+curso = "\"Ultimate \nPython\""
 print(curso)
