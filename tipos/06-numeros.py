68568295c32302d9f536b87b1b1c4e34e6e2d042
balemansteve <balemansteve@gmail.com>
2024-08-26 23:48:52 -0300

    numeros

--- tipos/06-numeros.py
+++ tipos/06-numeros.py
@@ -2,17 +2,17 @@ numero = 2 # entero -> integer
 decimal = 1.2 # float
 imaginario = 2 + 2j # 2 + 2i -> j seria el numero imaginario
 
-# numero = numero + 2
-numero += 5
-numero *= 5
-numero -= 5
-numero /= 5
+# Operadores aritméticos a variables
+numero += 5 # numero = numero + 5
+numero *= 5 # numero = numero * 5
+numero -= 5 # numero = numero - 5
+numero /= 5 # numero = numero / 5
 
-print("numero", numero)
-print(1 + 3)
-print(1 - 3)
-print(1 * 3)
-print(1 / 3)
-print(1 // 3)
-print(8 % 3)
-print(2 ** 3)
+print("numero", numero) 
+print(1 + 3) # suma
+print(1 - 3) # resta
+print(1 * 3) # multiplicación
+print(1 / 3) # división
+print(1 // 3) # división entera
+print(8 % 3) # módulo
+print(2 ** 3) # potencia
