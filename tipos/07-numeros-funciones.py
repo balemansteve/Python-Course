53d0df70222c25e80249d450d15fddf428d07a07
balemansteve <balemansteve@gmail.com>
2024-08-27 22:20:05 -0300

    numeros-funciones

--- tipos/07-numeros-funciones.py
+++ tipos/07-numeros-funciones.py
@@ -1,12 +1,12 @@
-import math
+import math # Importamos la librería math (tiene funciones matemáticas)
 
-print(round(1.3))
+print(round(1.3)) # Redondea al entero más cercano
 print(round(1.7))
 print(round(1.5))
-print(abs(55))
+print(abs(55)) # Valor absoluto
 
-print(math.ceil(1.1))
-print(math.floor(1.9999))
-#print(math.isnan("23"))
-print(math.pow(10, 3))
-print(math.sqrt(9))
+print(math.ceil(1.1)) # Redondea al entero más grande
+print(math.floor(1.9999)) # Redondea al entero más pequeño
+#print(math.isnan("23")) # No es un número
+print(math.pow(10, 3)) # Potencia pow(base, exponente)
+print(math.sqrt(9)) # Raíz cuadrada
