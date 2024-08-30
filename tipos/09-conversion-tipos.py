4eb4b6776283c3047997d8b5a9d6136ae5283716
balemansteve <balemansteve@gmail.com>
2024-08-29 23:57:40 -0300

    conversion-tipos

--- tipos/09-conversion-tipos.py
+++ tipos/09-conversion-tipos.py
@@ -1,12 +1,13 @@
-x = input("")
+x = input("") # input devuelve un string
 
 # int()
 # str()
 # float()
 # bool()
 
-print(bool(""))
-print(bool("0"))
-print(bool(None))
-print(bool(" "))
-print(bool(0))
+print(bool("")) # imrpime False porque la cadena esta vacia
+print(bool("0")) # imprime True porque la cadena tiene un 0
+print(bool(None)) # imprime False porque None es un valor nulo
+print(bool(" ")) # imprime True porque la cadena tiene un espacio
+print(bool(0)) # imprime False porque el numero es 0
+print(bool(1)) # imprime True porque el numero es 1
