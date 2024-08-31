a57770d2b64196359cefd955f10ec1d621e0571c
balemansteve <balemansteve@gmail.com>
2024-08-31 16:23:54 -0300

    if

--- control-flujo/02-if.py
+++ control-flujo/02-if.py
@@ -1,11 +1,12 @@
-edad = 15
+# Condicionales if, elif, else
+edad = 17
 if edad > 65:
     print("Puede ver la pelicula")
     print("Tienes super descuento")
 elif edad > 55:
     print("Puede ver la pelicula")
     print("Tienes descuento")
-elif edad > 17:
+elif edad >= 17:
     print("Puede ver la pelicula")
 else:
     print("Debes irte")
