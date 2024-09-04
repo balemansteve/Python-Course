8484e4026629fff17398d3c78d478bc95e00f1cb
Bryan Aleman <balemansteve@gmail.com>
2024-09-03 23:40:18 -0300

    for

--- control-flujo/06-for.py
+++ control-flujo/06-for.py
@@ -1,5 +1,7 @@
 edad = 10
 
+# Iterar sobre una lista de números y comparar con la edad, si la edad es encontrada, imprimir un mensaje y salir del ciclo con break
+# range() genera una lista de números desde 0, la cantidad de números es el argumento que se le pasa
 for numero in range(10):
     print(numero)
     if numero == edad:
@@ -7,6 +9,7 @@ for numero in range(10):
         break
 else:
     print("Edad no encontrada")
-    
+
+# Iterar sobre un string e imprimir cada caracter
 for char in "Ultimate Python":
     print(char)
