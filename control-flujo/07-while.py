1a032e5ef0127b3ace4293ab55ea9d1d8be55aa3
Bryan Aleman <balemansteve@gmail.com>
2024-09-05 23:46:59 -0300

    while

--- control-flujo/07-while.py
+++ control-flujo/07-while.py
@@ -3,8 +3,9 @@
 #     print(numero)
 #     numero *= 2
 
-comando = ""
+comando = "" # Inicializar la variable comando
 
+# Mientras el comando no sea "salir", imprimir el comando. lower() convierte el string a minúsculas para comparar con "salir" correctamentef
 while comando.lower() != "salir":
     comando = input("$ ")
     print(comando)
