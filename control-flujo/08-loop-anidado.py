9e0ddd0bc3e0ec5b2c12b72296c9231838d3fe29
Bryan Aleman <balemansteve@gmail.com>
2024-09-07 00:54:02 -0300

    loop-anidado

--- control-flujo/08-loop-anidado.py
+++ control-flujo/08-loop-anidado.py
@@ -1,3 +1,4 @@
+# Bucle anidado es un bucle dentro de otro bucle, primero se ejecuta el bucle interno y luego vuelva a ejecutar el bucle externo
 for j in range(3):
     for k in range(2):
         print(f"{j}, {k}")
