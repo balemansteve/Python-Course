f1a805ac352db815ce174fb6028797545cb47bdf
Bryan Aleman <balemansteve@gmail.com>
2024-09-08 23:51:11 -0300

    calculadora

--- control-flujo/09 -calculadora.py
+++ control-flujo/09 -calculadora.py
@@ -1,3 +1,5 @@
+# Programa que simula una calculadora, donde el usuario ingresa un numero, una operacion y otro numero, y el programa muestra el resultado de la operacion. 
+# El programa se ejecuta en un bucle infinito hasta que el usuario ingresa la palabra "salir".
 print("Bienvenidos a la calculadora")
 print("Para salir escribe salir")
 print("Las operaciones son suma, resta, multi y div")
