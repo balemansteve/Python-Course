60c7f8c244cb732abf45f220abb7496b766c6795
Bryan Aleman <balemansteve@gmail.com>
2024-09-17 22:12:09 -0300

    Spelling fixed

--- control-flujo/09 -calculadora.py
+++ control-flujo/09 -calculadora.py
@@ -1,4 +1,4 @@
-# Programa que simula una calculadora, donde el usuario ingresa un numero, una operación y otro numero, y el programa muestra el resultado de la operación
+# Programa que simula una calculadora, donde el usuario ingresa un número, una operación y otro número, y el programa muestra el resultado de la operación
 # El programa se ejecuta en un bucle infinito hasta que el usuario ingresa la palabra "salir"
 print("Bienvenidos a la calculadora")
 print("Para salir escribe salir")
@@ -7,14 +7,14 @@ print("Las operaciones son suma, resta, multi y div")
 resultado = ""
 while True:
     if not resultado:
-        resultado = input("Ingrese numero: ")
+        resultado = input("Ingrese número: ")
         if resultado.lower() == "salir":
             break
         resultado = int(resultado)
     operador = input("Ingrese la operación: ")
     if operador.lower() == "salir":
         break
-    num2 = input("Ingrese el siguiente numero: ")
+    num2 = input("Ingrese el siguiente número: ")
     if num2.lower() == "salir":
         break
     num2 = int(num2)
