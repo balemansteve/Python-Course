35e7fff2d5185f911dc6e4781a25f0325d5c3b52
Bryan Aleman <balemansteve@gmail.com>
2024-09-30 23:59:16 -0300

    Test

---
+++ prueba.py
@@ -0,0 +1,16 @@
+class Animal:
+    def __init__(self, nombre, edad):
+        self.name = nombre
+        self.age = edad
+    
+    def correr(self):
+        print(f"{self.name} está corriendo")
+        
+    def dormir(self):
+        print(f"{self.name} está durmiendo")
+    
+    pass
+
+perro = Animal("Firulais, 10")
+
+print(f"{perro.name} tiene {perro.age} años")
