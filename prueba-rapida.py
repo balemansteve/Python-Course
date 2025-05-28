f9d45faa54ac9eef4f4a721a1ba99fd2569f7c2e
balemansteve <balemansteve@gmail.com>
2025-05-28 15:18:17 -0300

    loop test

---
+++ prueba-rapida.py
@@ -0,0 +1,11 @@
+nombres = ["Ale", "Bryan", "Maria"]
+notas = [10, 8, 6]
+
+ranking = []
+for rank, (nombre, nota) in enumerate(zip(nombres, notas), start=1):
+    ranking.append(f"{rank}. {nombre}: {nota}")
+print(ranking)
+
+
+
+# f"{rank}. {nombre}: {nota}"
