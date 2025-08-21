82918d9bf7f042d1feb77b40d82e13f7afed1bac
Bryan Aleman <balemansteve@gmail.com>
2025-08-20 23:53:40 -0300

    Update 01-comparadores.py

--- control-flujo/01-comparadores.py
+++ control-flujo/01-comparadores.py
@@ -1,3 +1,7 @@
+"""
+Modulo que muestra los comparadores lógicos
+"""
+
 # Comparadores logicos
 print(1 > 2)
 print(1 < 2)
@@ -10,3 +14,4 @@ print(2 == 3)
 print(2 == "2")
 print(2 != "2")
 print(2 != 2)
+
