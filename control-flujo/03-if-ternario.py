aa569c7fffe4833770beb2621b5122fd34d1e5ae
Bryan Aleman <balemansteve@gmail.com>
2025-08-26 23:17:17 -0300

    Reformat and clarify ternary operator example

--- control-flujo/03-if-ternario.py
+++ control-flujo/03-if-ternario.py
@@ -1,6 +1,7 @@
 edad = 15
 
-# El operador ternario es una forma de simplificar un if-else, donde se evalúa una condición y se asigna un valor si es verdadera y otro si es falsa
+# El operador ternario es una forma de simplificar un if-else, 
+donde se evalúa una condición y se asigna un valor si es verdadera y otro si es falsa
 mensaje = "Es mayor" if edad > 17 else "Es menor"
 
 # if edad > 17:
@@ -9,3 +10,4 @@ mensaje = "Es mayor" if edad > 17 else "Es menor"
 #     mensaje = "Es menor"
     
 print(mensaje)
+
