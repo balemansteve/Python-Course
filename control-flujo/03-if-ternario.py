79465db602f0001b6dd609b82f1d5b87096bd286
Bryan Aleman <balemansteve@gmail.com>
2025-09-02 23:58:29 -0300

    Add explanation for ternary operator usage

--- control-flujo/03-if-ternario.py
+++ control-flujo/03-if-ternario.py
@@ -1,3 +1,11 @@
+"""
+Se utiliza un operador ternario para asignar un valor a la variable 'mensaje'.
+El operador ternario evalúa la condición 'edad > 17':
+    - Si es True, asigna "Es mayor"
+    - Si es False, asigna "Es menor"
+Esta forma es equivalente al bloque if-else comentado más abajo.
+"""
+
 edad = 15
 
 # El operador ternario es una forma de simplificar un if-else, 
@@ -10,4 +18,5 @@ mensaje = "Es mayor" if edad > 17 else "Es menor"
 #     mensaje = "Es menor"
     
 print(mensaje)
+
 
