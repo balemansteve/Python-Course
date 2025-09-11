a50c671e5c840551e6d878a1a65b9d5169c37309
Bryan Aleman <balemansteve@gmail.com>
2025-09-10 23:35:31 -0300

    Add documentation for suma function with *args

--- funciones/02-xargs.py
+++ funciones/02-xargs.py
@@ -1,3 +1,15 @@
+"""
+Ejemplo de función con número variable de argumentos (*args):
+
+- Se define la función suma(*numeros), donde *numeros permite recibir
+  cualquier cantidad de argumentos como una tupla.
+- Dentro de la función, se recorren los valores y se van sumando en 'resultado'.
+- Finalmente, se imprime la suma total.
+
+- Se llama a la función con diferentes cantidades de argumentos, 
+  mostrando que *args se adapta a cada caso.
+"""
+
 # Definimos una función que recibe un número variable de argumentos, suma los números y los imprime. Llamamos a la función con diferentes cantidades de argumentos
 def suma(*numeros):
     resultado = 0
@@ -9,3 +21,4 @@ def suma(*numeros):
 suma(2, 5, 7)
 suma(2, 8)
 suma(2, 8, 9, 10)
+
