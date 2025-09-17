755b7ea77ff959cc80ddb8a18f468539ac5dce8d
Bryan Aleman <balemansteve@gmail.com>
2025-09-16 22:32:32 -0300

    Add example for global variable scope in functions

--- funciones/05-alcance.py
+++ funciones/05-alcance.py
@@ -1,3 +1,17 @@
+"""
+Ejemplo de variable global y su modificación dentro de funciones:
+
+- Se define una variable global 'saludo' con el valor "Hola Python".
+- La función saludar() usa la palabra clave 'global' para indicar que
+  modificará la variable global, cambiando su valor a "Hola Mundo".
+- La función saludaChanchito() crea una variable local llamada 'saludo'
+  (diferente de la global) y no afecta el valor global.
+- En la ejecución:
+    1. Se imprime la variable global ("Hola Python").
+    2. Se llama a saludar(), que cambia la variable global.
+    3. Se imprime de nuevo mostrando el nuevo valor ("Hola Mundo").
+"""
+
 # Definimos una variable global
 saludo = "Hola Python"
 
@@ -14,3 +28,4 @@ def saludaChanchito():
 print(saludo)
 saludar()
 print(saludo)
+
