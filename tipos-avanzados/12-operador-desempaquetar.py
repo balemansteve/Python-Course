6cf01308b990b7a4ed62298a598accd5eeb0c690
Bryan Aleman <balemansteve@gmail.com>
2025-10-09 23:24:00 -0300

    Agregar ejemplos de operadores de desempaquetado en Python
    Este programa muestra cómo utilizar los operadores de desempaquetado * y ** en Python, incluyendo ejemplos de desempaquetado de listas, tuplas y diccionarios.

--- tipos-avanzados/12-operador-desempaquetar.py
+++ tipos-avanzados/12-operador-desempaquetar.py
@@ -1,3 +1,18 @@
+"""
+Este programa muestra cómo utilizar los operadores de desempaquetado * y ** en Python:
+
+1. El operador * desempaqueta listas o tuplas:
+   - Permite imprimir o combinar elementos de forma individual.
+   - Se puede usar junto con otros valores para crear nuevas listas o tuplas.
+
+2. El operador ** desempaqueta diccionarios:
+   - Combina varios diccionarios en uno solo.
+   - Si hay claves repetidas, se conserva el valor del último diccionario desempaquetado.
+   - También puede combinarse con claves adicionales personalizadas.
+
+Finalmente, se imprimen los resultados de las combinaciones.
+"""
+
 # * sirve para desempaquetar una lista o tupla, es decir, para obtener los elementos de una lista o tupla de forma individual.
 lista = [1, 2, 3, 4, 5]
 print(*lista)
@@ -19,3 +34,4 @@ print({**punto1, **punto2})
 # Se puede combinar con elementos adicionales
 nuevoPunto = {**punto1, "hola": "mundo", **punto2}
 print(nuevoPunto)
+
